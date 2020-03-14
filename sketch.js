4975775c841e60077ba4b7666eb6ab06590f7f7a
Max <max.09@outlook.xom>
2020-03-14 14:16:15 +0100

    made a grid

--- sketch.js
+++ sketch.js
@@ -1,16 +1,50 @@
 // Reference : https://p5js.org/reference/
 
-
+let colorOn;
+let colorOff;
+let canva;
+let canvaWitdh = window.screen.availWidth/2;
+let canvaHeight = window.screen.availHeight/2;
+let grid
 function setup() {
-    createCanvas(640, 480);
-    background(200)
+    colorOn =  color(10, 10, 10);
+    colorOff = color("white");
+    canva = createCanvas(canvaWitdh, canvaHeight);
+    canva.mouseClicked(changeColor);
+    background(200);
+
+    grid = initGrid(10,20);
+    console.table(grid)
   }
   
   function draw() {
     if (mouseIsPressed) {
-      fill(0);
+      fill(colorOn);
     } else {
-      fill(random(256));
+      fill(colorOff);
     }
     ellipse(mouseX, mouseY, 80, 80);
+  }
+
+  function initGrid(nbrCols, nbrRows) {
+    let grid = new Array(nbrCols);
+    for (let i = 0; i < nbrCols; i++) {
+      grid[i] = new Array(nbrRows)
+    }
+    fillGrid(grid)
+    return grid;
+  }
+  function fillGrid(grid){
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j <grid[0].length; j++) {
+          grid[i][j] = 0
+        }        
+      }
+  }
+
+  function mouseClicked(){
+    console.log("mouse click detected")
+  }
+  function changeColor(){
+    colorOn = color(random(256),random(256),random(256))
   }
